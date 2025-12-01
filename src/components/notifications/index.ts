4f737fbee9269d76b4e91aa4eca28ac04c5420e7
nebstarmalala <nebstartrizzle@gmail.com>
2025-12-01 23:07:21 +0300

    Add notifications components exports

---
+++ src/components/notifications/index.ts
@@ -0,0 +1,4 @@
+export { NotificationBell } from "./NotificationBell"
+export { NotificationDropdown } from "./NotificationDropdown"
+export { NotificationList } from "./NotificationList"
+export { NotificationItem } from "./NotificationItem"
