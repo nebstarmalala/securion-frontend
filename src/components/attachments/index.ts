776eaa9a16747a550ee0018eaec74871ea028ce9
nebstarmalala <nebstartrizzle@gmail.com>
2025-12-01 23:06:39 +0300

    Add attachment components exports

---
+++ src/components/attachments/index.ts
@@ -0,0 +1,4 @@
+export { AttachmentSection } from "./AttachmentSection"
+export { AttachmentUpload } from "./AttachmentUpload"
+export { AttachmentList } from "./AttachmentList"
+export { AttachmentCard } from "./AttachmentCard"
