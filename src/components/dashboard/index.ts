044e5eda32f7bd61c1d434c4e5a105f50e2c4639
nebstarmalala <nebstartrizzle@gmail.com>
2025-12-01 23:06:56 +0300

    Add dashboard components exports

---
+++ src/components/dashboard/index.ts
@@ -0,0 +1,22 @@
+/**
+ * Dashboard Components Export
+ * Phase 4: Dashboard & Home Experience
+ */
+
+export { QuickActions, QuickActionsCompact } from "./QuickActions"
+export { RecentlyViewed, RecentlyViewedCompact } from "./RecentlyViewed"
+export { OnboardingChecklist, OnboardingProgress } from "./OnboardingChecklist"
+export { TesterDashboard, ManagerDashboard, AdminDashboard } from "./RoleDashboard"
+export {
+  EmptyState,
+  NoProjectsEmpty,
+  NoFindingsEmpty,
+  NoScopesEmpty,
+  NoCVEsEmpty,
+  NoReportsEmpty,
+  NoUsersEmpty,
+  NoActivityEmpty,
+  NoSearchResultsEmpty,
+  NoNotificationsEmpty,
+  InlineEmpty,
+} from "./EmptyStates"
