b5f342f44a8250afe4cccf70ffbc3ebe2c683a5a
nebstarmalala <nebstartrizzle@gmail.com>
2025-12-04 00:42:04 +0300

    Add navigation components exports

---
+++ src/components/navigation/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Navigation Components Export
+ * Phase 3: Simplified Routing & Breadcrumbs
+ */
+
+export { Breadcrumbs, MobileBreadcrumb } from "./Breadcrumbs"
+export type { BreadcrumbItemProps } from "./Breadcrumbs"
+
+export { BackButton, BackLink } from "./BackButton"
+
+export { QuickJump, QuickJumpInline } from "./QuickJump"
+
+export { KeyboardShortcutsHelp, ShortcutHint } from "./KeyboardShortcutsHelp"
